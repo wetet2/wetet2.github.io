a692e3ef5badd12ee9c4136a6e37d99a5d92774e
Hoyoon Lee <wetet2@gmail.com>
2021-08-22 22:10:28 +0900

    first commit

---
+++ library/Slider.js
@@ -0,0 +1,83 @@
+'use strict';
+/**
+ * Made by HoyoonLee
+ * @param {*} options 
+ */
+function Slider(options){
+    let $$ = this;
+    let _isPointerClicked = false;
+
+    let $slider = $(options.selector);
+    let $template =$(
+        '<div class="slider-wrap">'+
+            '<div class="back-bar"></div>'+
+            '<div class="bar"><div class="pointer"></div></div>'+
+        '</div>');
+    $slider.html($template);
+    $slider.attr('title', options.title || '');
+
+    let $sliderWrap = $slider.find('.slider-wrap');
+    let $backBar = $slider.find('.back-bar');
+    let $bar = $slider.find('.bar');
+    let $pointer = $slider.find('.pointer');
+
+    let _backBarWidth = $backBar.width();
+    let _minVal = $slider.attr('minVal');
+    let _maxVal = $slider.attr('maxVal');
+
+    $(document)
+        .on('mouseup', function(e){
+            if(_isPointerClicked){
+                _isPointerClicked = false;
+            }
+        });
+    $sliderWrap
+        .on('mousemove',function(e){
+            if(_isPointerClicked){
+                $$.setPointer($bar.width() + e.originalEvent.movementX);
+                // console.log(e.originalEvent.movementX);
+            }
+        });
+    $pointer
+        .on('mousedown',function(e){
+            if(e.which === 1){
+                _isPointerClicked = true;
+            }
+            e.stopPropagation();
+        });
+
+    $backBar
+        .on('mousedown', function(e){
+            if(e.which === 1){
+                $$.setPointer(e.offsetX);
+            }
+            e.stopPropagation();
+        });
+
+    $bar
+        .on('mousedown', function(e){
+            if(e.which === 1){
+                $$.setPointer(e.offsetX);
+            }
+            e.stopPropagation();
+        });
+
+    $$.setPointer = function(px){
+        if(px <= _backBarWidth && px >= 0){
+            $bar.css('width', px);
+            // $pointer.css('left', px);
+            let val = parseFloat((_maxVal - _minVal) * (px / _backBarWidth)).toFixed(2);
+            $slider.attr('val', val);
+            options.pointChanged(val);
+        }
+    };
+
+    $$.setValue = function(val){
+        $$.setPointer(_backBarWidth * (val / _maxVal));
+    }
+
+
+    $$.setValue(parseFloat($slider.attr('val')));
+
+
+}
