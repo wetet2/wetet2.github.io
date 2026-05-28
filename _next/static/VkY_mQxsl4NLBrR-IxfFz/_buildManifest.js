8d4a7536ac7a9bf85a329c2268cc5ef9afcd06bf
Hoyoon Lee <wetet2@gmail.com>
2026-05-29 08:31:00 +0900

    또 수정2

---
+++ _next/static/VkY_mQxsl4NLBrR-IxfFz/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0~_skpl1lu57-.js"
+  ],
+  "/_error": [
+    "static/chunks/12.65ha.n3tjc.js"
+  ],
+  "/d": [
+    "static/chunks/11y6x~rit82~3.js"
+  ],
+  "/g": [
+    "static/chunks/16qhyxd.p6g0i.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/d",
+    "/g"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/11y6x~rit82~3.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/d", [
+  "static/chunks/0glab_74t5f19.js",
+  "static/chunks/043n1z~v_jj_j.js",
+  "static/chunks/0-d0giwii2tay.js",
+  "static/chunks/184uu_5xaf7h3.js",
+  "static/chunks/turbopack-0upvq03dno486.js"
+])
