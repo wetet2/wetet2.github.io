82925123ec8075dae19e93039d54ca01f848f963
Hoyoon Lee <wetet2@gmail.com>
2026-06-19 00:50:12 +0900

    텍스트위치 조정

---
+++ _next/static/AaK_sRORLOS1kcMtTuiV3/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/17mxk_s_2ew1x.js"
+  ],
+  "/_error": [
+    "static/chunks/16dmjsud9zyp_.js"
+  ],
+  "/d": [
+    "static/chunks/18c9ovmdlu0my.js"
+  ],
+  "/g": [
+    "static/chunks/0sfq9ywey~fam.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/d",
+    "/g"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/18c9ovmdlu0my.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/d", [
+  "static/chunks/0.hmtfxkm538f.js",
+  "static/chunks/07irefnzycfnd.js",
+  "static/chunks/0uh6icr6baq9d.js",
+  "static/chunks/13lt52h_nk.vi.js",
+  "static/chunks/184uu_5xaf7h3.js",
+  "static/chunks/turbopack-043_kpe9.ltr5.js"
+])
