88c11a82402d60ac09947fb365e769b50768aa0e
Hoyoon Lee <wetet2@gmail.com>
2026-06-19 07:17:35 +0900

    텍스트 드래깅 추가

---
+++ _next/static/chunks/0b7bj~_a0xaa..js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/d", [
+  "static/chunks/0.hmtfxkm538f.js",
+  "static/chunks/07irefnzycfnd.js",
+  "static/chunks/0uh6icr6baq9d.js",
+  "static/chunks/0qs6c55949g8_.js",
+  "static/chunks/184uu_5xaf7h3.js",
+  "static/chunks/turbopack-0o3-rbph-7hoa.js"
+])
---
+++ _next/static/tqhKPvRevTo8Xx6Tv20Mj/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/17mxk_s_2ew1x.js"
+  ],
+  "/_error": [
+    "static/chunks/16dmjsud9zyp_.js"
+  ],
+  "/d": [
+    "static/chunks/0b7bj~_a0xaa..js"
+  ],
+  "/g": [
+    "static/chunks/0sfq9ywey~fam.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/d",
+    "/g"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
