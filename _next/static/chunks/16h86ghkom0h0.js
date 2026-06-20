b2517895d3b552fb72c7897c0d8e02be48576591
Hoyoon Lee <wetet2@gmail.com>
2026-06-21 08:04:42 +0900

    모바일 처리

---
+++ _next/static/chunks/16h86ghkom0h0.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/c", [
+  "static/chunks/0kqk4ftiko.q0.js",
+  "static/chunks/161-2gc6caswm.js",
+  "static/chunks/184uu_5xaf7h3.js",
+  "static/chunks/0g_25uwiqcnxn.js",
+  "static/chunks/turbopack-0k6rik._x_~91.js"
+])
