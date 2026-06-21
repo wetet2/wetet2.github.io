eae727a52d758541b7c05643de9cd478761a90cb
Hoyoon Lee <wetet2@gmail.com>
2026-06-21 21:56:01 +0900

    벽 겹침 수정

---
+++ _next/static/WFQSxU5Ty2fUieekMSsqM/_buildManifest.js
@@ -0,0 +1,30 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/118x974h9djm5.js"
+  ],
+  "/_error": [
+    "static/chunks/0xzdwtmc2gff0.js"
+  ],
+  "/c": [
+    "static/chunks/14pq1.2ze2~d1.js"
+  ],
+  "/d": [
+    "static/chunks/013-e7szeax03.js"
+  ],
+  "/g": [
+    "static/chunks/0rct-q49uvmtv.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/c",
+    "/d",
+    "/g"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
