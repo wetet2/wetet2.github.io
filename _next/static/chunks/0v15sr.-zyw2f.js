ed5dea575caa0d1a127dba82e08faa5004d2dade
Hoyoon Lee <wetet2@gmail.com>
2026-06-23 23:26:38 +0900

    회전,크기 핸들러 우선 적용. seo 처리 등

---
+++ _next/static/chunks/0v15sr.-zyw2f.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/d", [
+  "static/chunks/0kqk4ftiko.q0.js",
+  "static/chunks/0fbtnq1c8kiun.js",
+  "static/chunks/0g_25uwiqcnxn.js",
+  "static/chunks/0_f.6dgw340l4.js",
+  "static/chunks/184uu_5xaf7h3.js",
+  "static/chunks/turbopack-0sskpmpkmyafl.js"
+])
