c4e890fa9ff6b4406f11b88962432179a96b835a
Hoyoon Lee <wetet2@gmail.com>
2026-06-27 16:44:38 +0900

    홈화면 정리

---
+++ _next/static/AK19v5G6cCCSqqLtn5wFw/_buildManifest.js
@@ -0,0 +1,30 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0qmku~ou0jtsx.js"
+  ],
+  "/_error": [
+    "static/chunks/0dws0f3fcblne.js"
+  ],
+  "/c": [
+    "static/chunks/11d88-dwa~5ir.js"
+  ],
+  "/d": [
+    "static/chunks/0e6-fuolwr9tw.js"
+  ],
+  "/g": [
+    "static/chunks/14ivdfxdyg4r5.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/c",
+    "/d",
+    "/g"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
