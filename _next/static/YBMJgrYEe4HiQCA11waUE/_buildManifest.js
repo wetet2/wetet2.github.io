48f3dd8556c4e9aef7e3041509b2462b599859b2
Hoyoon Lee <wetet2@gmail.com>
2026-09-07 14:14:52 +0900

    지뢰찾기 개선

--- _next/static/YBMJgrYEe4HiQCA11waUE/_buildManifest.js
+++ _next/static/YBMJgrYEe4HiQCA11waUE/_buildManifest.js
@@ -6,7 +6,7 @@ self.__BUILD_MANIFEST = {
     "static/chunks/0vb.6ua95m77y.js"
   ],
   "/b": [
-    "static/chunks/0f~2c5eh143_t.js"
+    "static/chunks/028x07namqnqj.js"
   ],
   "/c": [
     "static/chunks/18b29bj2rm-jk.js"
--- _next/static/chunks/028x07namqnqj.js
+++ _next/static/chunks/028x07namqnqj.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/b", [
-  "static/chunks/0bge_v_.x6-5s.js",
+  "static/chunks/0upj4i0ugb4au.js",
   "static/chunks/0mkrc0qcv22r-.js",
   "static/chunks/0x4c0_avl76nc.js",
   "static/chunks/0zttme1szwyjw.js",
   "static/chunks/0uw~ov22uo1v0.js",
-  "static/chunks/turbopack-0-m-iwbh18z19.js"
+  "static/chunks/turbopack-0gwjd.6_wd~ba.js"
 ])
